c648ddea0782f731de9183f1a04f3d010e40b06e
johnbeilly85-ctrl <johnbeilly85@gmail.com>
2026-09-12 15:17:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,30 @@ app.post("/ussd", (req, res) => {
 1. Buy Data Bundle
 2. Check Balance
 3. Customer Support`);
-  } else {
+  } 
+  else if (text === "1") {
+    res.send(`CON Buy Data Bundle
+1. Ksh 100 - 45GB (Not Expiring)
+2. Ksh 250 - 100GB (Not Expiring)
+3. Ksh 500 - 1000GB (Not Expiring)
+0. Back`);
+  } 
+  else if (text === "1*1") {
+    res.send("END You selected Ksh 100 - 45GB (Not Expiring).");
+  } 
+  else if (text === "1*2") {
+    res.send("END You selected Ksh 250 - 100GB (Not Expiring).");
+  } 
+  else if (text === "1*3") {
+    res.send("END You selected Ksh 500 - 1000GB (Not Expiring).");
+  } 
+  else if (text === "1*0") {
+    res.send(`CON Welcome to Best Data Offer
+1. Buy Data Bundle
+2. Check Balance
+3. Customer Support`);
+  } 
+  else {
     res.send("END Thank you.");
   }
 });
