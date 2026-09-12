5ff625e8d709a0501a420ef5bee7b50b5c06d05d
johnbeilly85-ctrl <johnbeilly85@gmail.com>
2026-09-12 17:04:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,22 @@ app.post("/ussd", (req, res) => {
   }
 
   if (text === "1") {
-    return res.send(`END Choose your package:
-Ksh 100 - 45GB
-Ksh 250 - 100GB
-Ksh 500 - 1000GB`);
+    return res.send(`CON Choose your package:
+1. Ksh 100 - 45GB
+2. Ksh 250 - 100GB
+3. Ksh 500 - 1000GB`);
+  }
+
+  if (text === "1*1") {
+    return res.send("END You selected Ksh 100 - 45GB. M-Pesa payment will be requested.");
+  }
+
+  if (text === "1*2") {
+    return res.send("END You selected Ksh 250 - 100GB. M-Pesa payment will be requested.");
+  }
+
+  if (text === "1*3") {
+    return res.send("END You selected Ksh 500 - 1000GB. M-Pesa payment will be requested.");
   }
 
   res.send("END Invalid option");
